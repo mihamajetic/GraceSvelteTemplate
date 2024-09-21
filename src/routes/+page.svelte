d5565dea56024a2873566270a1ed143aaa030b07
LongarMD <mark.longar@gmail.com>
2024-09-21 17:43:25 +0200

    Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let x = 0;
+
+	function onClick() {
+		x = x + 1;
+	}
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<button class="p-2 bg-blue-500 text-white rounded-lg" on:click={onClick}>
+	Kliknil si me {x}-krat
+</button>
