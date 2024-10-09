9d2f063fa84500c695bf4957f0e7647d328f27af
Miha Majetič <miha@zerodays.dev>
2024-10-09 18:41:04 +0200

    koala

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,31 @@
-<div class="grid justify-items-center gap-5">
-	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
-		Hello World, Hello Grace!
-	</h1>
-	<img class="rounded-md w-96" src="art.webp" />
+<div class="flex justify-center">
+	<div class="rounded-xl shadow-md w-8/12 m-10 p-5 bg-teal-100 flex flex-col items-center">
+		<h1 class="text-3xl font-bold">Koale</h1>
+		<h3 class="text-xl font-bold italic">Phascolarctos cinereus</h3>
+		<img src="koala.jpg" class="w-5/12 p-5" />
+		<p class="py-2">
+			Koala (znanstveno ime Phascolarctos cinereus) je rastlinojedi drevesni vrečar, ki izvira iz
+			Avstralije in je edini danes živeči predstavnik družine Phascolarctidae.
+		</p>
+		<p class="py-2">
+			Koale bivajo v vzhodni in jugovzhodni Avstraliji v evkaliptovih gozdovih. So samotarske
+			živali, ki so pretežno aktivne zvečer in ponoči. V primeru vremenskih skrajnosti se ne
+			zatekajo v skrivališča, pač pa jih pred tem ščiti kožuh z dobrimi izolacijskimi lastnostmi,
+			hladijo se predvsem s počivanjem na deblih, ki so hladnejša od okolice.
+		</p>
+		<p class="py-2">
+			So specializirane živali, ki se prehranjujejo z evkaliptovimi listi. Ta hrana ima razmeroma
+			malo energijsko bogatih snovi, je težko prebavljiva in vsebuje strupene presnovke, zaradi
+			česar so se pri koalah razvile številne prilagoditve telesa, presnove in prebave ter
+			vedenjskih vzorcev. Telo in okončine so prilagojene življenju na drevju, zaradi varčevanja z
+			energijo večino dneva prespijo ter se izogibajo energijsko potratnemu vedenju; boji med samci
+			so pogostejši le med paritveno sezono.
+		</p>
+		<a href="#"
+			><button
+				class="rounded-full bg-yellow-400 text-white px-10 py-2 hover:bg-yellow-900 uppercase font-bold"
+				>Preberi več</button
+			></a
+		>
+	</div>
 </div>
