9747ae9bdd4d505d3b23d91ba6eb9cae36b85f2e
Miha Majetič <miha@zerodays.dev>
2024-12-11 15:24:48 +0100

    config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	safelist: [
+		'text-black',
 		// Automatically generate all text color combinations
 		...['red', 'green', 'blue', 'yellow', 'purple'].flatMap((color) =>
 			[500, 600, 700, 800, 900].map((shade) => `text-${color}-${shade}`)
